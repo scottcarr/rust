350853677d533aa2ccdf79049aaefd9ef52bcaf0
Scott A Carr <s.carr1024@gmail.com>
2016-06-20 14:11:08 -0700

    start impl of move up

--- src/librustc_mir/transform/move_up_propagation.rs
+++ src/librustc_mir/transform/move_up_propagation.rs
@@ -12,35 +12,108 @@ use rustc::ty::TyCtxt;
 use rustc::mir::repr::*;
 use rustc::mir::transform::{MirPass, MirSource, Pass};
 use rustc_data_structures::indexed_vec::{Idx, IndexVec};
+use rustc::mir::visit::{Visitor, LvalueContext};
+use std::collections::HashMap;
 
 struct MoveUpPropagation;
 
 impl<'tcx> MirPass<'tcx> for MoveUpPropagation {
     fn run_pass<'a>(&mut self, _tcx: TyCtxt<'a, 'tcx, 'tcx>, _src: MirSource, mir: &mut Mir<'tcx>) {
-        for bb in mir.basic_blocks_mut() {
-            for (i, stmt) in bb.statements().iter().enumerate() {
-                if let Statement(_, 
-                    StatementKind(Assign(lval, TempDecl(tmpId)))) = stmt {
-                    try_optimze(i, bb);
+        let tuc = TempUseCounter::new(mir);
+        let candidates = tuc.counts.iter().filter(|&(tmp, &cnt)| cnt == 1);
+        for c in candidates {
+            // do something
+        }
+        // for bb in mir.basic_blocks_mut() {
+        //     for (i, stmt) in bb.statements().iter().enumerate() {
+        //         if let Statement(_, 
+        //             StatementKind(Assign(lval, TempDecl(tmpId)))) = stmt {
+        //             try_optimze(i, bb);
+        //         }
+        //     }
+        // }
+    }
+}
+
+// enum StopCondition {
+//     DontStop,
+//     TempBorrowed,
+//     TempUsed,
+//     FoundDef,
+// }
+
+// fn try_optimze(stmt_idx: usize, bb: BasicBlock) {
+//     walk_backwards(stmt_idx, bb);
+// }
+
+// fn walk_backwards(stmt_idx: usize, bb: BasicBlock) -> StopCondition {
+//     let bail = StopCondition::DontStop;
+//     while stmt_idx >= 0 and StopCondition::DontStop == bail {
+//         bail = visit(bb.statements[stmt_idx]);
+//         stmt_idx -= 1;
+//     }
+//     if stmt_idx == -1 {
+//         for p in mir.predecessors_for(bb) {
+//             bail = walk_backwards(p.statements.len()-1, p)
+//             if bail { break; }
+//         }
+//     }
+//     bail
+// }
+
+// fn walk_forwards(stmt_idx: usize, bb: BasicBlock) -> StopCondition {
+//     let bail = StopCondition::DontStop;
+//     while stmt_idx < bb.statements.len() and StopCondition::DontStop == bail {
+//         bail = visit(bb.statements[stmt_idx]);
+//         stmt_idx -= 1;
+//     }
+//     if stmt_idx == bb.statements.len() - 1 {
+//         for p in mir.predecessors_for(bb) {
+//             bail = walk_forwards(0, p)
+//             if bail { break; }
+//         }
+//     }
+//     bail
+// }
+
+// fn visit(stmt: Statement) -> StopCondition {
+//     StopCondition::Success
+// }
+
+impl Pass for MoveUpPropagation {}
+
+struct TempUseCounter {
+    pub counts: HashMap<Temp, usize>,
+}
+
+impl TempUseCounter {
+    fn new (mir: &Mir) -> Self {
+        let mut tuc = TempUseCounter { counts: HashMap::new() };
+        tuc.visit_mir(mir);
+        tuc
+    }
+}
+impl<'a> Visitor<'a> for TempUseCounter {
+    fn visit_lvalue(&mut self,
+                    lvalue: &Lvalue<'a>,
+                    context: LvalueContext) {
+        match context {
+            LvalueContext::Store => { /* a store is a def, not a use*/ },
+            //LvalueContext::Call => {},
+            //LvalueContext::Drop => {},
+            //LvalueContext::Inspect => {},
+            //LvalueContext::Borrow { region: Region, kind: BorrowKind },
+            //LvalueContext::Slice { from_start: usize, from_end: usize },
+            //LvalueContext::Projection => {},
+            //LvalueContext::Consume => {},
+            _ => {
+                if let &Lvalue::Temp(tmp_id) = lvalue {
+                    *self.counts.entry(tmp_id).or_insert(0) += 1;
                 }
             }
         }
     }
 }
-
-fn try_optimze(stmt_idx: usize, bb: BasicBlock) {
-}
-
-fn walk_backwards(stmt_idx: usize, bb: BasicBlock) {
-    if stmt_idx > 0 {
-        stmt_idx -= 1
-    } else {
-
-    }
-}
-
-impl Pass for MoveUpPropagation {}
-
 // one potential problem with only looking at
 // statements is what if the temporary I'm eliminating
 // is used in a terminator?

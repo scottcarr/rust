093e46d437aaf555356d952f1973465376b7d820
Scott A Carr <s.carr1024@gmail.com>
2016-06-20 17:06:59 -0700

    gather def use on all temporaries

--- src/librustc_mir/transform/move_up_propagation.rs
+++ src/librustc_mir/transform/move_up_propagation.rs
@@ -11,22 +11,33 @@
 use rustc::ty::TyCtxt;
 use rustc::mir::repr::*;
 use rustc::mir::transform::{MirPass, MirSource, Pass};
-use rustc_data_structures::indexed_vec::{Idx, IndexVec};
+// use rustc_data_structures::indexed_vec::{Idx, IndexVec};
 use rustc::mir::visit::{Visitor, LvalueContext};
 use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 
-struct MoveUpPropagation;
+pub struct MoveUpPropagation;
 
 impl<'tcx> MirPass<'tcx> for MoveUpPropagation {
-    fn run_pass<'a>(&mut self, _tcx: TyCtxt<'a, 'tcx, 'tcx>, _src: MirSource, mir: &mut Mir<'tcx>) {
-        let tuc = TempUseCounter::new(mir);
-        let candidates = tuc.counts.iter().filter(|&(tmp, &cnt)| cnt == 1);
-        for c in candidates {
+    fn run_pass<'a>(&mut self,
+                    _tcx: TyCtxt<'a, 'tcx, 'tcx>,
+                    _src: MirSource,
+                    mir: &mut Mir<'tcx>) {
+        let tuc = TempDefUseFinder::new(mir);
+        let candidates = tuc.uses.iter().filter(|&(_, ref uses)| uses.len() == 1);
+        for (&tmp, _) in candidates {
             // do something
+            debug!("{:?} has only one use!", tmp);
+            if let Some(v) = tuc.defs.get(&tmp) {
+                debug!("{:?} has {} defs", tmp, v.len());
+            } else {
+                debug!("we didn't have any defs for {:?}?", tmp);
+            }
+
         }
         // for bb in mir.basic_blocks_mut() {
         //     for (i, stmt) in bb.statements().iter().enumerate() {
-        //         if let Statement(_, 
+        //         if let Statement(_,
         //             StatementKind(Assign(lval, TempDecl(tmpId)))) = stmt {
         //             try_optimze(i, bb);
         //         }
@@ -82,38 +93,70 @@ impl<'tcx> MirPass<'tcx> for MoveUpPropagation {
 
 impl Pass for MoveUpPropagation {}
 
-struct TempUseCounter {
-    pub counts: HashMap<Temp, usize>,
+struct StatementLocation {
+    basic_block: BasicBlock,
+    statement_index: usize,
 }
 
-impl TempUseCounter {
-    fn new (mir: &Mir) -> Self {
-        let mut tuc = TempUseCounter { counts: HashMap::new() };
+struct TempDefUseFinder {
+    pub defs: HashMap<Temp, Vec<StatementLocation>>,
+    pub uses: HashMap<Temp, Vec<StatementLocation>>,
+    curr_basic_block: BasicBlock,
+    statement_index: usize,
+}
+
+impl TempDefUseFinder {
+    fn new(mir: &Mir) -> Self {
+        let mut tuc = TempDefUseFinder {
+            defs: HashMap::new(),
+            uses: HashMap::new(),
+            curr_basic_block: START_BLOCK,
+            statement_index: 0,
+        };
         tuc.visit_mir(mir);
         tuc
     }
 }
-impl<'a> Visitor<'a> for TempUseCounter {
-    fn visit_lvalue(&mut self,
-                    lvalue: &Lvalue<'a>,
-                    context: LvalueContext) {
+impl<'a> Visitor<'a> for TempDefUseFinder {
+    fn visit_basic_block_data(&mut self, block: BasicBlock, data: &BasicBlockData<'a>) {
+        self.curr_basic_block = block;
+        self.statement_index = 0;
+        self.super_basic_block_data(block, data);
+    }
+    fn visit_statement(&mut self, block: BasicBlock, statement: &Statement<'a>) {
+        self.super_statement(block, statement);
+        self.statement_index += 1;
+    }
+    fn visit_lvalue(&mut self, lvalue: &Lvalue<'a>, context: LvalueContext) {
         match context {
-            LvalueContext::Store => { /* a store is a def, not a use*/ },
-            //LvalueContext::Call => {},
-            //LvalueContext::Drop => {},
-            //LvalueContext::Inspect => {},
-            //LvalueContext::Borrow { region: Region, kind: BorrowKind },
-            //LvalueContext::Slice { from_start: usize, from_end: usize },
-            //LvalueContext::Projection => {},
-            //LvalueContext::Consume => {},
+            LvalueContext::Store => {
+                if let &Lvalue::Temp(tmp_id) = lvalue {
+                    self.defs.entry(tmp_id).or_insert(vec![]).push(StatementLocation {
+                        basic_block: self.curr_basic_block,
+                        statement_index: self.statement_index,
+                    });
+                }
+            }
+            // LvalueContext::Call => {},
+            // LvalueContext::Drop => {},
+            // LvalueContext::Inspect => {},
+            // LvalueContext::Borrow { region: Region, kind: BorrowKind },
+            // LvalueContext::Slice { from_start: usize, from_end: usize },
+            // LvalueContext::Projection => {},
+            // LvalueContext::Consume => {},
             _ => {
                 if let &Lvalue::Temp(tmp_id) = lvalue {
-                    *self.counts.entry(tmp_id).or_insert(0) += 1;
+                    self.uses.entry(tmp_id).or_insert(vec![]).push(StatementLocation {
+                        basic_block: self.curr_basic_block,
+                        statement_index: self.statement_index,
+                    });
                 }
             }
         }
+        self.super_lvalue(lvalue, context);
     }
 }
+
 // one potential problem with only looking at
 // statements is what if the temporary I'm eliminating
 // is used in a terminator?

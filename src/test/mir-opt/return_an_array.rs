f77485a03dc892b6c44404e76de9a5e9aef36fcd
Scott A Carr <s.carr1024@gmail.com>
2016-07-12 10:27:41 -0700

    add a good test for if simplification

--- src/test/mir-opt/return_an_array.rs
+++ src/test/mir-opt/return_an_array.rs
@@ -8,9 +8,11 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// this tests move up progration, which is not yet implemented
+
 fn foo() -> [u8; 1024] {
         let x = [0; 1024];
-            return x;
+        return x;
 }
 
 fn main() { }
